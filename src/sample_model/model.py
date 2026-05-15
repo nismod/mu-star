770c93842eb62af65c5ce7020cb25e3e347c5b08
Tom Russell <tom.russell@ouce.ox.ac.uk>
2026-05-15 12:19:45 +0100

    Add sample model package
    This is an initial idea for infrastructure system model implementation
    - that each sector (transport/energy/water) should provide a sub-package
    within this repository which exposes a uniform interface for the
    simulation of disruptions. Interface details tbd.

---
+++ src/sample_model/model.py
@@ -0,0 +1,12 @@
+"""Expose a standard model interface"""
+
+
+class SampleModel:
+    """Example model class"""
+
+    def __init__(self):
+        pass
+
+    def simulate(self, network, disruptions):
+        metrics = None
+        return metrics
---
+++ src/sample_model/tests/test_model.py
@@ -0,0 +1,10 @@
+from sample_model.model import SampleModel
+
+
+def test_example():
+    model = SampleModel()
+    network = None
+    disruptions = None
+    actual = model.simulate(network, disruptions)
+    expected = None
+    assert actual == expected
